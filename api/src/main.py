c9343ec4ba1d0302a40ff70b5486d69ff7c4e5b5
MriteshAdak <mriteshadak@outlook.com>
2026-03-05 22:23:03 +0100

    Fix import path for CORS middleware and add redirect_slashes option in FastAPI initialization

--- api/src/main.py
+++ api/src/main.py
@@ -3,13 +3,14 @@ from fastapi import FastAPI
 from src.infrastructure.lifecycle import lifespan
 from src.routers import tasks as tasks_router, system as system_router
 from src.exceptions.exception_handlers import register_exception_handlers
-from middleware.cors import register_cors_middleware
+from src.middleware.cors import register_cors_middleware
 
 app: FastAPI = FastAPI(
     title="Tasks API",
     description="API for managing tasks",
     version="1.1.0",
-    lifespan=lifespan)
+    lifespan=lifespan,
+    redirect_slashes=False)
 
 register_cors_middleware(app)
 register_exception_handlers(app)
