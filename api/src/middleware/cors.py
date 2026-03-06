669276ff43e56cf6f061e81838741d709d87560f
MriteshAdak <mriteshadak@outlook.com>
2026-03-06 07:28:58 +0100

    Refactor CORS middleware configuration to specify types for origins and restrict allowed methods and headers

--- api/src/middleware/cors.py
+++ api/src/middleware/cors.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from src.config import settings
+# from src.config import settings
 
 def register_cors_middleware(app: FastAPI) -> None:
     """
     Configures CORS middleware for the FastAPI application
     """
-    origins = [origin.strip() for origin in settings.ui_origins.split(",") if origin.strip()]
+    origins: list[str] = [origin.strip() for origin in settings.ui_origins.split(",") if origin.strip()]
     
     app.add_middleware(
         CORSMiddleware,
+        # allow_origins=["*"],
+        # allow_credentials=False,
+        # allow_origin_regex=r"https://.*\.vercel\.app",
         allow_origins=origins,
-        allow_origin_regex=r"https://.*\.vercel\.app",
         allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_methods=["GET, POST, PUT, DELETE"],
+        allow_headers=["application/json"],
     )
