7f89e679a328c7d31b1f056078dd1a30c9610906
MriteshAdak <mriteshadak@outlook.com>
2026-03-07 03:34:22 +0100

    Refactor environment variable handling and update CORS middleware to use new settings structure

--- api/src/middleware/cors.py
+++ api/src/middleware/cors.py
@@ -7,15 +7,17 @@ def register_cors_middleware(app: FastAPI) -> None:
     """
     Configures CORS middleware for the FastAPI application
     """
-    origins: list[str] = [origin.strip() for origin in settings.ui_origins.split(",") if origin.strip()]
+    # origins: list[str] = [origin.strip() for origin in settings.cors_origins if origin.strip()]
     
     app.add_middleware(
         CORSMiddleware,
         # allow_origins=["*"],
         # allow_credentials=False,
         # allow_origin_regex=r"https://.*\.vercel\.app",
-        allow_origins=origins,
+        allow_origins=settings.cors_origins,
         allow_credentials=True,
-        allow_methods=["GET, POST, PUT, DELETE"],
-        allow_headers=["application/json"],
+        # allow_methods=["GET, POST, PUT, DELETE"],
+        # allow_headers=["application/json"],
+        allow_headers=["*"],
+        allow_methods=["*"],
     )
